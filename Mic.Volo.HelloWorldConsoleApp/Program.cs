a7d2136b6cad38fd921d1a300835d9051cafb6c5
User <artyom.hovakimyan01@gmail.com>
2019-03-29 14:01:32 +0400

    Add CW

--- Mic.Volo.HelloWorldConsoleApp/Program.cs
+++ Mic.Volo.HelloWorldConsoleApp/Program.cs
@@ -10,9 +10,12 @@ namespace Mic.Volo.HelloWorldConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Homework");
             Console.WriteLine("Hello World!");
             Console.WriteLine($"{(Environment.ProcessorCount)}");
             Console.WriteLine("{0}",Environment.OSVersion);
+            Console.WriteLine($"{(Environment.Is64BitProcess ? "x64" : "x84")}");
+
 
         }
     }
