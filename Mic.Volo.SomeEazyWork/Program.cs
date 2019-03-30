17615d34d475f3a81504aeb0a561a31c7ca67a7f
User <artyom.hovakimyan01@gmail.com>
2019-03-30 20:52:18 +0400

    Add  new classes

--- Mic.Volo.SomeEazyWork/Program.cs
+++ Mic.Volo.SomeEazyWork/Program.cs
@@ -10,8 +10,15 @@ namespace Mic.Volo.SomeEazyWork
     {
         static void Main(string[] args)
         {
-            Car mycar = new Car();
-            mycar.PrintState();
+            List<Rectangle> myListOfRects = new List<Rectangle>
+            {
+                new Rectangle{TopLeft=new Point{X=10,Y=10},
+                BottomRight=new Point{X=200,Y=200}},
+                new Rectangle{TopLeft=new Point{X=2,Y=2},
+                BottomRight=new Point{X=100,Y=100}},
+                new Rectangle{TopLeft=new Point{X=5,Y=5},
+                BottomRight=new Point{X=90,Y=75}}
+            };
         }
     }
 }
---
+++ Mic.Volo.SomeEazyWork/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.SomeEazyWork
+{
+    class Rectangle
+    {
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int Tlx,int Tly,int Brx,int Bry)
+        {
+            TopLeft = new Point(Tlx, Tly);
+            BottomRight = new Point(Brx, Bry);
+        }
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+    }
+}
