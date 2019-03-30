625949c305d25abcc097210fe86dc3222dc603fe
User <artyom.hovakimyan01@gmail.com>
2019-03-30 17:47:43 +0400

    Update Car.cs

--- Mic.Volo.SomeEazyWork/Car.cs
+++ Mic.Volo.SomeEazyWork/Car.cs
@@ -8,5 +8,16 @@ namespace Mic.Volo.SomeEazyWork
 {
     class Car
     {
+        public string petName;
+        public int carrSpeed;
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.",petName,carrSpeed);
+        }
+        public void SpeedUP(int delta)
+        {
+            carrSpeed += delta;
+        }
+
     }
 }
