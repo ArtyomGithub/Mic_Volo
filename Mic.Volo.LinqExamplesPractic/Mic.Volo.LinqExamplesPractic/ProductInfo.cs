8fb293a0191fc541b6aac4e2f8804e07ca37efcd
User <artyom.hovakimyan01@gmail.com>
2019-04-01 15:10:27 +0400

    feat: add class productinfo

---
+++ Mic.Volo.LinqExamplesPractic/Mic.Volo.LinqExamplesPractic/ProductInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.LinqExamplesPractic
+{
+    class ProductInfo
+    {
+        public string Name { get; set; }
+        public string Descrition { get; set; }
+        public int NumberInStock { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Name={0}, Description={1}," +
+                "Number in Stock={2}", Name, Descrition, NumberInStock);
+        }
+    }
+}
