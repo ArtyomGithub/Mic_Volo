64e175d85fe5b18ed79142974473292dd0e09a98
User <artyom.hovakimyan01@gmail.com>
2019-04-02 17:50:12 +0400

    example int maxvalue fibonacci

---
+++ Mic.Volo.ToDoMy/Mic.Volo.ToDoMy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Numerics;
+
+namespace Mic.Volo.ToDoMy
+{
+    class Fibonacci
+    {
+        public int a = 0;
+        public int b = 1;
+        public int c;
+        public void Values()
+        {
+            Console.WriteLine(" "+a);
+            while (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+               
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci f = new Fibonacci();
+            while (true)
+            {
+                f.Values();
+            }
+        }
+    }
+}
