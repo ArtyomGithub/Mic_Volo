772f7e61cb1447c97cd878b1f50fe7e554d2a37c
User <artyom.hovakimyan01@gmail.com>
2019-04-06 01:59:57 +0400

    add Numerics.dll

---
+++ Mic.Volo.FibonacciBig/Mic.Volo.FibonacciBig/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Mic.Volo.FibonacciBig
+{
+    class Program
+    {
+        static IEnumerable<BigInteger> Fibonacci()
+        {
+            BigInteger a = new BigInteger(0);
+            BigInteger b = new BigInteger(1);
+            BigInteger c = new BigInteger(0);
+
+            while (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                yield return c;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        static void Main(string[] args)
+        {
+            IEnumerable<BigInteger> Values = Fibonacci();
+            foreach (var V in Values)
+            {
+                Console.Write("Press Spacebar for Next value:");
+                Console.WriteLine(V);
+            }
+
+        }
+    }
+}
