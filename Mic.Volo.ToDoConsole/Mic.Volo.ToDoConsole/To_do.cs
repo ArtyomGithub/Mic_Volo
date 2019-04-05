0e5183fc6458b7a1f958eb535d84a2e534eceb15
User <artyom.hovakimyan01@gmail.com>
2019-04-05 14:10:25 +0400

    add frontend some functions

---
+++ Mic.Volo.ToDoConsole/Mic.Volo.ToDoConsole/To_do.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.ToDoConsole
+{
+    class To_do:IComparable<To_do>
+    {
+        public int Todo_ID { get; set; }
+        public DateTime date { get; set; }
+        public string Task { get; set; }
+        public int Lvl_Imp { get; set; }
+        public To_do(int todo_ID, DateTime date, string task, int lvl_Imp)
+        {
+            Todo_ID = todo_ID;
+            this.date = date;
+            Task = task;
+            Lvl_Imp = lvl_Imp;
+        }
+
+        public int CompareTo(To_do other)
+        {
+            return this.Todo_ID.CompareTo(other.Todo_ID);
+        }
+    }
+}
