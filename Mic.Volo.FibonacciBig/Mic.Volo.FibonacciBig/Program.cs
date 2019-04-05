0814476dfc69508f249de44ec7a981ce70e4e980
User <artyom.hovakimyan01@gmail.com>
2019-04-06 02:46:37 +0400

    Group Numbers

--- Mic.Volo.FibonacciBig/Mic.Volo.FibonacciBig/Program.cs
+++ Mic.Volo.FibonacciBig/Mic.Volo.FibonacciBig/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
+using System.Collections;
 
 namespace Mic.Volo.FibonacciBig
 {
@@ -25,11 +26,84 @@ namespace Mic.Volo.FibonacciBig
         }
         static void Main(string[] args)
         {
-            IEnumerable<BigInteger> Values = Fibonacci();
-            foreach (var V in Values)
+
+            List<Phone> phones = new List<Phone>
+{
+    new Phone {Name="Lumia 430", Company="Microsoft" },
+    new Phone {Name="Mi 5", Company="Xiaomi" },
+    new Phone {Name="LG G 3", Company="LG" },
+    new Phone {Name="iPhone 5", Company="Apple" },
+    new Phone {Name="Lumia 930", Company="Microsoft" },
+    new Phone {Name="iPhone 6", Company="Apple" },
+    new Phone {Name="Lumia 630", Company="Microsoft" },
+    new Phone {Name="LG G 4", Company="LG" }
+};
+
+            var phoneGroups = from phone in phones
+                              group phone by phone.Company;
+
+            foreach (IGrouping<string,Phone> item in phoneGroups)
             {
-                Console.Write("Press Spacebar for Next value:");
-                Console.WriteLine(V);
+                Console.WriteLine(item.Key);
+                foreach (var t in item)
+                {
+                    Console.WriteLine(t.Name);
+                }
+                Console.WriteLine();
+            }
+            
+            //var collection = Enumerable.Range(0, 1000);
+            //var res = from i in collection
+            //          group i by i.ToString().Count() into g
+            //          select new
+            //          {
+            //              Group = g.Key,
+            //              Value = g
+            //          };
+
+            //foreach(IGrouping<int,int> v in res)
+            //{
+            //    Console.WriteLine(v.Key);
+            //    foreach (var item in v)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            //var result = collection.GroupBy(i => i.ToString().Length).
+            //    Select(i=>new { Name=i.Key,Val=i.ToString().Count()});
+
+            //foreach (var group in result)
+            //{
+            //    Console.WriteLine("{0}:{1}",group.Name,group.Val);
+            //    foreach (var item in group.Val)
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //}
+            var collection = Enumerable.Range(0, 1000);
+            var groups = collection.GroupBy(num => num.ToString().Length);
+            //foreach (var group in groups)
+            //{
+            //    Console.WriteLine($"Group #{group.Key}");
+            //    foreach (var num in group)
+            //    {
+            //        Console.Write(num + "  ");
+            //    }
+
+            //    Console.WriteLine();
+            //    Console.ReadKey();
+            //}
+            var collection = Enumerable.Range(0, 1000);
+            var numgroup = from i in collection
+                           group i by i.ToString().Length;
+            foreach (IGrouping<int,int> item in numgroup)
+            {
+                Console.WriteLine($"#{item.Key}");
+                foreach (var t in item)
+                {
+                    Console.Write(t);
+                }
+                Console.WriteLine();
             }
 
         }
