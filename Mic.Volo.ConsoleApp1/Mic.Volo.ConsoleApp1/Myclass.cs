f54594b2cba9de6fbf665c828199c332b4d96bf9
User <artyom.hovakimyan01@gmail.com>
2019-04-06 21:24:07 +0400

    add work with GC

---
+++ Mic.Volo.ConsoleApp1/Mic.Volo.ConsoleApp1/Myclass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.ConsoleApp1
+{
+    class Myclass
+    {
+        int speed;
+        string name;
+
+        public Myclass(int speed, string name)
+        {
+            this.speed = speed;
+            this.name = name;
+        }
+    }
+}
--- Mic.Volo.ConsoleApp1/Mic.Volo.ConsoleApp1/Program.cs
+++ Mic.Volo.ConsoleApp1/Mic.Volo.ConsoleApp1/Program.cs
@@ -27,25 +27,45 @@ namespace Mic.Volo.ConsoleApp1
             }
             Console.WriteLine("*********");
 
-            using (WebClientMy ob = new WebClientMy())
-            {
-                ob.DownloadHTML("https://mic.am");
-            }
+            //using (WebClientMy ob = new WebClientMy())
+            //{
+            //    ob.DownloadHTML("https://mic.am");
+            //}
 
-            WebClient web = new WebClient();
-            using (Stream stream = web.OpenRead("https://norvig.com/big.txt"))
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    string line = "";
-                    while((line=reader.ReadLine())!=null)
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-            Console.WriteLine("File downloaded");
+            //WebClient web = new WebClient();
+            //using (Stream stream = web.OpenRead("https://norvig.com/big.txt"))
+            //{
+            //    using (StreamReader reader = new StreamReader(stream))
+            //    {
+            //        string line = "";
+            //        while((line=reader.ReadLine())!=null)
+            //        {
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("File downloaded");
+            Console.WriteLine("Heap memory :"+GC.GetTotalMemory(false));
+            Console.WriteLine("Os object generation :"+(GC.MaxGeneration));
+            Myclass ob = new Myclass(10, "one");
+            Console.WriteLine("ob is {0}",GC.GetGeneration(ob));
 
+            GC.Collect();
+            Console.WriteLine("ob is {0}",GC.GetGeneration(ob));
+
+            object[] Arrayob = new object[5000000];
+            for (int i = 0; i < 1000000; i++)
+            {
+                Arrayob[i] = new object();
+            }
+            GC.Collect(0);
+            GC.WaitForPendingFinalizers();
+            Console.WriteLine("ob is {0}",GC.GetGeneration(ob));
+            GC.Collect();
+            Console.WriteLine("\n Gen 0 has benn swept {0} times",GC.CollectionCount(0));
+            Console.WriteLine("Gen 1 has been swept {0} times",GC.CollectionCount(1));
+            Console.WriteLine("Gen 2 has been swept {0} times",GC.CollectionCount(2));
+            Console.WriteLine("Heap memory :"+GC.GetTotalMemory(false));
         }
     }
 }
